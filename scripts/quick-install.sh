b63de289ead0857eb33a0c5da8721983736a7808
Nikita Zaitsev (exluap) <nickzaytsew@gmail.com>
2017-06-09 15:02:41 +0300

    Updated quick install

--- scripts/quick-install.sh
+++ scripts/quick-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Installing pacman's packages"
-sudo pacman -S conky ranger zsh rofi ncmpcpp nitrogen xterm spectacle git ttf-droid
+sudo pacman -S conky ranger zsh rofi ncmpcpp nitrogen xterm spectacle git ttf-droid networkmanager network-manager-applet dhclient networkmanager-openvpn gnome-keyring
 echo "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
